bfbc86a9c1582cc36d1921f8837cf4a0df57de77
Swaz002 <swastikraychoudhuri@gmail.com>
2022-07-02 19:55:38 -0700

    added a few functionalities

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
-            description: "Soft comportable socks .Unisex both for male and female",
             image: "assets/images/socks_green.jpg",
-            url: "https://www.youtube.com/"
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id:2234, color: 'green', image: 'assets/images/socks_green.jpg' },
+                { id:2235, color: 'blue', image: 'assets/images/socks_blue.jpg' }
+            ],
+        }
+    },
+    methods:{
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
