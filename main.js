5b18538309ffadab69bb7592a36189e48a27f1d8
Swaz002 <swastikraychoudhuri@gmail.com>
2022-07-02 18:09:42 -0700

    first vue app

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: "Soft comportable socks .Unisex both for male and female"
+        }
+    }
+})
