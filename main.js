9bddd9217b043a3b8c357dde8ced78d461d9f8e0
Swaz002 <swastikraychoudhuri@gmail.com>
2022-07-07 01:50:54 -0700

    commiting before a mess

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             brand: "Vue Mastery",
-            image: "assets/images/socks_green.jpg",
+            onSale : true,
+            selectedVariant: 0,
             inventory: 1000,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id:2234, color: 'green', image: 'assets/images/socks_green.jpg' },
-                { id:2235, color: 'blue', image: 'assets/images/socks_blue.jpg' }
+                { id:2234, color: 'green', image: 'assets/images/socks_green.jpg', quantity:100 },
+                { id:2235, color: 'blue', image: 'assets/images/socks_blue.jpg', quantity: 0 }
             ],
         }
     },
@@ -17,8 +18,26 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed:{
+        title() {
+            return this.brand + "\n" + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + " " + this.product + " " + "is on sale."
+            }
+            return ''
         }
     }
 })
